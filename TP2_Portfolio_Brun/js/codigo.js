38ade2d7ed1b259cce548de7074e09da2f420c12
malenabrun <135130796+malenabrun@users.noreply.github.com>
2023-06-14 00:04:51 -0300

    Add files via upload

---
+++ TP2_Portfolio_Brun/js/codigo.js
@@ -0,0 +1,14 @@
+document.getElementById('menu').addEventListener('click', function()
+{
+
+document.getElementById('navega').classList.toggle('mostrar');
+
+});
+
+function reproducir(){
+    document.getElementById('miVideo').play();
+    }
+    
+    function pausar(){
+        document.getElementById('miVideo').pause();
+    }
